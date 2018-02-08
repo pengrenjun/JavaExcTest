1c882d00efc0aec56f7a487273c7fe764cf5ef91
pengrenjun <809507790@qq.com>
2018-02-08 16:33:07 +0800

    泛型单例模式及Comparator接口在列表中的应用

--- FanXing/TestFx.java
+++ FanXing/TestFx.java
@@ -1,13 +1,28 @@
 package com.ronglian.fssc.webapp.FanXing;
 
+import com.google.common.collect.Maps;
+import org.apache.commons.collections.map.HashedMap;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class TestFx {
 
     public static void main(String[] args) {
         //创建无限制通配符的数组是可以的
         List<? >[] arr=new List<? >[]{};
+
+        //泛型静态工厂方法创建 Map集合 google的Maps类帮助创建了
+      /*  public static <K, V> HashMap<K, V> newHashMap() {
+            return new HashMap<K, V>();
+        }*/
+
+        Map<String,List<String>> map= Maps.newHashMap();
+
+
+
 
 
 
